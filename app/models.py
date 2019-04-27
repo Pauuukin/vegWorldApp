281cacf29c1389e332c18094111e6235b18cb5b2
pauuukin <oleg.paukin@mail.ru>
2019-04-27 15:42:48 +0200

    create table user, category, status and post

--- app/models.py
+++ app/models.py
@@ -1,7 +1,9 @@
+from datetime import datetime
 from app import db
 
+
 class User(db.Model):
-    idUser = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     fullName = db.Column(db.String(64))
     email = db.Column(db.String(120), unique=True)
     nickname = db.Column(db.String(32), unique = True)
@@ -10,7 +12,40 @@ class User(db.Model):
     gender = db.Column(db.String(32))
     aboutMe = db.Column(db.Text(600))
     dateOfBirth = db.Column(db.Date)
+    dateOfReg = db.Column(db.DateTime, index = True, default = datetime.utcnow)
+
 
     def __repr__(self):
         """Сообщает, как печатать объект класса"""
         return '<User {}>'.format(self.nickname)
+
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64))
+    parent_id = db.Column(db.Integer)
+
+
+    def __repr__(self):
+        """Сообщает, как печатать объект класса"""
+        return 'Category {}'.format(self.name)
+
+class Status(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(256))
+    text = db.Column(db.Text(500))
+
+    def __repr__(self):
+        """Сообщает, как печатать объект класса"""
+        return 'Status {}'.format(self.title)
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
+    headline = db.Column(db.String(256))
+    text = db.Column(db.Text(500))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    photo = db.Column(db.Text(500))
+    status_id = db.Column(db.Integer, db.ForeignKey('status.id'))
+    timestamp = db.Column(db.DateTime, index = True, default = datetime.utcnow)
